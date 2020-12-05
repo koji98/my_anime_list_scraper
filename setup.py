e92ecf038f0d829b277c0cb963df6438b021cb7e
chidiu98 <34973870+koji98@users.noreply.github.com>
2020-12-05 18:07:13 -0500

    Updating package version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as r:
 
 setuptools.setup(
     name="my_anime_list_scraper",
-    version="0.0.45",
+    version="0.0.46",
     author="Chidi Udeze",
     author_email="chidiu98@gmail.com",
     description="A scraper for www.myanimelist.com",
