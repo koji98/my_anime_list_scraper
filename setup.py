e8494cf17e91e65b29c9f20a1e338d9fd6dd8464
chidiu98 <34973870+koji98@users.noreply.github.com>
2020-12-05 00:21:05 -0500

    Updated package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="my_anime_list_scraper", # Replace with your own username
-    version="0.0.3",
+    version="0.0.4",
     author="Chidi Udeze",
     author_email="chidiu98@gmail.com",
     description="A scraper for www.myanimelist.com",
