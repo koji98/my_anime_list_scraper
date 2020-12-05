ff29a789b1058c6d2b4b03c72b339594543f8142
chidiu98 <34973870+koji98@users.noreply.github.com>
2020-12-05 15:57:26 -0500

    Still working on proper PyPi packaging

--- my_anime_list_scraper/__init__.py
+++ my_anime_list_scraper/__init__.py
@@ -1,7 +1,1 @@
-# from scraper import MalScraper
-# import cleaner
-# import extractor
-# import saver
-# import sql_queries
-# from scraper_types import PageTypes
-# from mysql_manager import DatabaseManager
+from scraper import MalScraper
--- setup.py
+++ setup.py
@@ -3,18 +3,22 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("LICENSE", "r") as fh:
+    license = fh.read()
+
 setuptools.setup(
     name="my_anime_list_scraper",
-    version="0.0.41",
+    version="0.0.42",
     author="Chidi Udeze",
     author_email="chidiu98@gmail.com",
     description="A scraper for www.myanimelist.com",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/koji98/my_anime_list_scraper",
-    py_modules=["scraper"],
-    # packages=setuptools.find_packages(),
-    package_dir={ '':'my_anime_list_scraper'},
+    license=license,
+    # py_modules=["scraper"],
+    packages=setuptools.find_packages(),
+    # package_dir={ '':'my_anime_list_scraper'},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
